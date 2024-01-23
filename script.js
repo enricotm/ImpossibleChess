03dab2706052ce499673f8472a1b2a0a8e7c8a24
Enrico Mazelli <enrico.mazellit@gmail.com>
2024-01-22 22:24:34 -0300

    func to calc flipCoin

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for (let row = 0; row < 8; row++) {
         coinDiv.classList.add("coinDiv");
 
         const coin = document.createElement("div");
-        coin.id = "coin"+(row*8+col);
+        coin.id = row*8+col;
         coin.classList.add("coin");
         coin.setAttribute("onclick", "ChangeCoin("+coin.id+")");
 
@@ -17,29 +17,33 @@ for (let row = 0; row < 8; row++) {
     mainDiv.appendChild(rowDiv);
 }
 let setKey = false;
+let confirmed = false;
 let keyId = 0;
 let playerMode = 2;
 Randomize();
 
 function Randomize() {
     for (let i = 0; i < 64; i++) {
-        const coin = document.getElementById("coin"+i);
+        const coin = document.getElementById(i);
         coin.classList.remove("red", "blue");
         coin.classList.add(["red", "blue"][Math.floor(Math.random()*2)]);
     }
     ChangeKey();
 }
 
-function ChangeCoin(coin) {
-    if (setKey) {
-        ChangeKey(parseInt(coin.id.replace("coin", "")));
+function ChangeCoin(coinId) {
+    if (confirmed) {
+    }
+    else if (setKey) {
+        ChangeKey(coinId);
         SetKey();
     } else {
-        ChangeColor(coin);
+        ChangeColor(coinId);
     }
 }
 
-function ChangeColor(coin) {
+function ChangeColor(coinId) {
+    const coin = document.getElementById(coinId)
     if (coin.classList.value.includes("red")) {
         coin.classList.remove("red");
         coin.classList.add("blue");
@@ -50,9 +54,9 @@ function ChangeColor(coin) {
 }
 
 function ChangeKey(coinId="") {
-    document.getElementById("coin"+keyId).parentElement.classList.remove("key");
+    document.getElementById(keyId).parentElement.classList.remove("key");
     keyId = coinId != "" ? coinId : Math.floor(Math.random()*64);
-    if (playerMode != 1) { document.getElementById("coin"+keyId).parentElement.classList.add("key") }
+    if (playerMode != 1) { document.getElementById(keyId).parentElement.classList.add("key") }
 }
 
 function SetKey() {
@@ -69,7 +73,7 @@ function SetKey() {
 function ChangePlayerMode() {
     playerMode = (playerMode % 2) + 1;
     const playerModeBtn = document.getElementById("playerMode");
-    const keyDiv = document.getElementById("coin"+keyId).parentElement;
+    const keyDiv = document.getElementById(keyId).parentElement;
     const setKeyBtn = document.getElementById("keyBtn");
     if (playerMode == 1) {
         playerModeBtn.textContent = "1 Player";
@@ -80,4 +84,53 @@ function ChangePlayerMode() {
         keyDiv.classList.add("key");
         setKeyBtn.hidden = false;
     }
+}
+
+function ConfirmBtn() {
+    if (confirmed) {
+        Randomize();
+        if (setKey) { SetKey() };
+        document.getElementById("configButtons").hidden = false;
+        document.getElementById("confirmBtn").textContent = "Confirm";
+        confirmed = false;
+    } else {
+        document.getElementById("configButtons").hidden = true;
+        document.getElementById("confirmBtn").textContent = "Restart";
+        confirmed = true;
+        calcCoinToFlip();
+    }
+}
+
+function calcCoinToFlip() {
+    let flipCoin = 0;
+    for (let i = 0; i < 64; i++) {
+        const coin = document.getElementById(i);
+        if (coin.classList.value.includes("red")) {
+            flipCoin = xor(flipCoin, parseInt(coin.id).toString(2));
+        };
+    }
+    flipCoin = xor(flipCoin, parseInt(keyId).toString(2));
+    flipCoinAnimation(flipCoin)
+}
+
+function flipCoinAnimation(flipCoin) {
+    const flipCoinId = parseInt(flipCoin, 2)
+    const flipDiv = document.getElementById(flipCoinId).parentElement;
+    ChangeColor(flipCoinId)
+    flipDiv.classList.add("flip");
+}
+
+function xor(val1, val2) {
+    val1 = String(val1); val2 = String(val2);
+    const n = Math.max(val1.length, val2.length);
+    val1 = val1.padStart(n,"0"); val2 = val2.padStart(n,"0");
+    let res = "";
+    for (let i = 0; i < n; i++) {
+        if (val1[i] != val2[i]) {
+            res += "1"
+        } else {
+            res += "0"
+        };
+    }
+    return res;
 }
